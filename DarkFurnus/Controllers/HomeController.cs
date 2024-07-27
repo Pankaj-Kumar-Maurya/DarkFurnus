1ec454f28e93de7c10202ccd78e43a7c476e0516
Pankaj Kumar Maurya <pankajmaurya872691@gmail.com>
2024-07-27 13:11:25 +0530

    Home page , css and assets setup in new branch Testing_Branch_1 and push in it.

--- DarkFurnus/Controllers/HomeController.cs
+++ DarkFurnus/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ namespace DarkFurnus.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        //First push test
+        
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
