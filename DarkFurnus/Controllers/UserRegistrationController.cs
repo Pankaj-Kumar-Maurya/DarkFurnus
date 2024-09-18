996122033f3203c1a253162f0a55b4923351cb6a
Pankaj Kumar Maurya <pankajmaurya872691@gmail.com>
2024-09-18 20:33:23 +0530

    UI , Landing page login and registration page basic complete

---
+++ DarkFurnus/Controllers/UserRegistrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DarkFurnus.Controllers
+{
+    public class UserRegistrationController : Controller
+    {
+        public IActionResult RegistrationView()
+        {
+            return View();
+        }
+    }
+}
