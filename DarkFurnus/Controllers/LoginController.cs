1c091f7937f6795e07eb72610ab9e20b119a1e6d
Pankaj Kumar Maurya <pankajmaurya872691@gmail.com>
2024-11-03 18:44:16 +0530

    Email for registration , uniqe done

--- DarkFurnus/Controllers/LoginController.cs
+++ DarkFurnus/Controllers/LoginController.cs
@@ -6,16 +6,21 @@ namespace DarkFurnus.Controllers
     public class LoginController : Controller
     {
         private readonly DarkFurnusDbContext _context;
+        private bool isRememberMeChecked;
+
         public LoginController(DarkFurnusDbContext context)
         {
             this._context = context;
         }
         public IActionResult LoginView()
         {
+            // Retrieve email and password from cookies if they exist
+            ViewBag.Email = Request.Cookies["email"];
+            ViewBag.Password = Request.Cookies["password"];
             return View();
         }
         [HttpPost]
-        public IActionResult LoginVerify(string email, string password)
+        public IActionResult LoginVerify(string email, string password, bool isRememberMeChecked)
         {
             // Query the TblUserRegistration table to find a user by email
             var user = _context.TblUserRegistrations.SingleOrDefault(u => u.Email == email);
@@ -23,16 +28,30 @@ namespace DarkFurnus.Controllers
             if (user == null)
             {
                 // User not found
-                TempData["ToastrMessage"] = "Invalid email";
-                return View("LoginView");
+                TempData["ToastrType"] = "error";
+                TempData["ToastrMessage"] = "No user fonded with this email";
+                return RedirectToAction("LoginView");
             }
 
             // Compare the provided password with the user's stored password
             if (user.Password != password)
             {
                 // Invalid password
-                TempData["ToastrMessage"] = "Invalid password.";
-                return View("LoginView");
+                TempData["ToastrType"] = "error";
+                TempData["ToastrMessage"] = "Wrong password.";
+                return RedirectToAction("LoginView");
+            }
+            //Set cookies if "Remember Me" is checked
+            if (isRememberMeChecked)
+            {
+                Response.Cookies.Append("email", email, new CookieOptions { Expires = DateTimeOffset.Now.AddDays(30) });
+                Response.Cookies.Append("password", password, new CookieOptions { Expires = DateTimeOffset.Now.AddDays(30) });
+            }
+            else
+            {
+                // Clear cookies if "Remember Me" is unchecked
+                Response.Cookies.Delete("email");
+                Response.Cookies.Delete("password");
             }
 
             // If the email and password are valid, proceed (e.g., log in the user)
